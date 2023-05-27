e28592b6d8177088604efe3a3d9ce86496bc5b65
sanaullahsabere <you@example.com>
2023-05-27 14:47:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function updateClock() {
+  var currentTime = new Date();
+
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+
+  
+  hours = (hours < 10 ? "0" : "") + hours;
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+
+  
+  var formattedTime = hours + ":" + minutes + ":" + seconds;
+
+  
+  document.getElementById("clock").textContent = formattedTime;
+}
+
+
+updateClock();
+
+
+setInterval(updateClock, 1000);
